81172605d861698d8c0799886a9a9aac9f847f5d
song <gkwlak645@gmail.com>
2022-04-15 17:43:14 +0900

    2.2 단원 진행

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,11 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const socket = io();
 
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ⭕");
-});
-
-socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
-});
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected to Server ❌");
-});
-
-messageForm.addEventListener("submit", (event) => {
+form.addEventListener("submit", event => {
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    const input = form.querySelector("input");
+    socket.emit("enter_room", { payload: input.value }, () => {alert("enter the room")});
     input.value = "";
 });
-
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import http from "http"
-import express from "express"
-import webSocket from "ws"
+import http from "http";
+import express from "express";
+import socketio from "socket.io";
 
 const app = express();
 
@@ -10,24 +10,54 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new webSocket.Server({server});
+const httpServer = http.createServer(app);
+const wsServer = socketio(httpServer);
 
+wsServer.on("connect", socket => {
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 3000)
+    });
+});
+
+httpServer.listen(3000, handleListen);
+
+/*
+import webSocket from "ws"
+const wss = new webSocket.Server({server});
 const sockets = [];
 
 wss.on("connection", (socket) => {
+    socket["nickname"] = "Unknown";
     sockets.push(socket);
     console.log("Connected to Browser ⭕");
     socket.on("close", () => console.log("Disconnected from Browser ❌"));
-    socket.on("message", (message) => {
-        sockets.forEach((aSocket) => {
-            aSocket.send(message.toString("utf8"));
-        });
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type) {
+            case "new_message":
+                for(let i = 0; i < sockets.length; i++) {
+                    if(socket === sockets[i])
+                        continue;
+                    sockets[i].send(`${sockets[i].nickname}: ${message.payload}`);
+                }
+                break;
+            case "nickname":
+                const bn = socket.nickname;
+                socket["nickname"] = message.payload;
+                socket.send("nickname changed: " + bn + " -> " + socket.nickname);
+                break;
+            default:
+                console.log("정의되지 않은 타입입니다.", message.type);
+                break;
+        }
     });
-    socket.send("hello!!!");
 });
 
-server.listen(3000, handleListen);
+server.listen(3000, handleListen); 
+*/
+
